3fac70370b9bad954e9444d9575adc7b6bd38d91
Jerrol Krause <jerrolkrause@gmail.com>
2016-02-29 13:29:33 -0800

    Moved contact logic from controller to route

--- app/models/contact.js
+++ app/models/contact.js
@@ -1,6 +1,17 @@
 import DS from 'ember-data';
+import Ember from 'ember';
 
 export default DS.Model.extend({
-  email		: DS.attr('string'),
-  message	: DS.attr('string')
+
+	email			: DS.attr('string'),
+	message			: DS.attr('string'),
+	hasResponse		: DS.attr('string'),
+
+	//Checks if an email address is present
+	validEmail		: Ember.computed.notEmpty('email'),
+	//Checks if the textarea has at least 5 characters typed
+	validMessage	: Ember.computed.gte('message.length', 5),
+	//Checks if both a valid email and a valid message are valid
+	isDisabled		: Ember.computed.not('validMessage', 'validEmail')
+
 });
--- app/routes/contact.js
+++ app/routes/contact.js
@@ -1,7 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+
 	model() {
-	    return this.store.findAll('contact');
+	    return this.store.createRecord('contact'); //Needs to be createRecord to pull from the model
+	},
+
+	actions: {
+		//On contact form submit
+		submit : function(model){
+
+			//Save model to server then set hasResponse to true
+		 	model.save().then(() => this.controller.set('hasResponse', true));
+
+		}//end submit
 	}
+
 });
