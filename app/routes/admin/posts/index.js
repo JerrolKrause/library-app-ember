57328e37be61c2bdd253a6900044b7aaa586e69a
Jerrol Krause <jerrolkrause@gmail.com>
2016-03-03 13:48:24 -0800

    Added blog logic to route

--- app/routes/admin/posts/index.js
+++ app/routes/admin/posts/index.js
@@ -4,6 +4,7 @@ export default Ember.Route.extend({
 	
 	model: function() { 
 		//Get posts and them sort them by postDate
-	    return this.store.findAll('post').then(posts => posts.sortBy('postDate:desc'));
+	    return this.store.findAll('post').then(posts => posts.sortBy('postDate'));
 	}
+
 });
